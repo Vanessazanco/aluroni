96caff13ea0b70443f678b78286cc0eea15256cb
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-04 10:35:21 +0100

    Routes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,8 @@
 import { ReactComponent as Logo } from 'assets/logo.svg';
 import styles from './Menu.module.scss';
+import {Link} from 'react-router-dom';
+
+
 export default function Menu() {
   const rotas= [{
     label:'Inicio',
@@ -17,9 +20,9 @@ export default function Menu() {
       <ul className={styles.menu__list}>
         {rotas.map((rota,index) =>(
           <li key={index} className={styles.menu__link}>
-            <a href={rota.to}>
+            <Link to={rota.to}>
               {rota.label}
-            </a>
+            </Link>
           </li>
         )
         
